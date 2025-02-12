e436d39dec949faacde7f47b3d7978868720d9bd
Khachik Khachatryan <khachik.khachatryan@softconstruct.com>
2025-02-12 14:23:38 +0000

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Используй '/' если сайт в корне GitHub Pages, иначе './' для корректных путей
+  base: '/khachik14700.github.io/', // Обратите внимание на корректный путь
 })
